efe753e544436c4df2d4147208a43c57de1a9af1
Millertaker <millerigac@hotmail.com>
2018-07-15 13:55:03 -0500

    Test for ham menu done

--- test/main/hamburguerMenu.js
+++ test/main/hamburguerMenu.js
@@ -7,6 +7,7 @@ import chai from 'chai';
 import {JSDOM} from 'jsdom';
 import homePage from '../mocks/homePage';
 import hamburgerMenu from '../../src/js/components/hamburgerMenu';
+import simulant from 'jsdom-simulant';
 
 
 describe('Nav Exercise test', () => {  
@@ -15,8 +16,17 @@ describe('Nav Exercise test', () => {
   global.document = dom.window.document;
   global.window = dom.window;
 
+  //event
+  var clickEvent = simulant(window, 'click');
+  const hamburgerMenuButton = document.getElementById('menu-toggle');
+
 	hamburgerMenu('nav-wrapper', 'masking-mobile', 'content-wrapper');
   it('Hamburger menu open', () => {
-    
+    var navWrapper = document.getElementById('nav-wrapper');
+  	simulant.fire(hamburgerMenuButton, clickEvent);
+
+  	assert.equal(navWrapper.getAttribute("class"), 'open-mobile', 'Mobile menu has opened');
+  	assert.equal(hamburgerMenuButton.getAttribute("class"), 'mobile-menu close', 'Ham menu icon has changed');
+  	
   });
 });
